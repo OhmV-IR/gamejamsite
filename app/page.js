850f21869a16408784bf5b0052e056d51ad88f20
Adrien Bourdeaux <bourdeaux.adrien@gmail.com>
2025-06-16 12:54:37 -0400

    fix build

--- app/page.js
+++ app/page.js
@@ -1,62 +1,63 @@
 import { IconCalendar, IconClipboardCheck, IconHome, IconInfoCircle, IconJetpack, IconLayoutDashboard, IconUserSearch } from "@tabler/icons-react";
+import Link from "next/link"
 
 export default function Home() {
   return (
     <div id="homepageroot">
       <header className="navbar navbar-expand-md d-print-none">
         <div className="container-xl">
-          <a href="/" aria-label="SiteLogo" className="navbar-brand navbar-brand-autodark me-3">
+          <Link href="/" aria-label="SiteLogo" className="navbar-brand navbar-brand-autodark me-3">
             <IconJetpack></IconJetpack>
             <h1>GameJam Name</h1>
-          </a>
+          </Link>
           <ul className="navbar-nav">
           <li className="nav-item active">
-        <a className="nav-link" href="/">
+        <Link className="nav-link" href="/">
           <span className="nav-link-icon">
             <IconHome></IconHome>
           </span>
           <span className="nav-link-title"> Home </span>
-        </a>
+        </Link>
       </li>
       <li className="nav-item">
-        <a className="nav-link" href="/aboutus">
+        <Link className="nav-link" href="/aboutus">
           <span className="nav-link-icon">
             <IconInfoCircle></IconInfoCircle>
           </span>
           <span className="nav-link-title"> About us </span>
-        </a>
+        </Link>
       </li>
       <li className="nav-item">
-        <a className="nav-link" href="/eventdetails">
+        <Link className="nav-link" href="/eventdetails">
           <span className="nav-link-icon">
             <IconCalendar></IconCalendar>
           </span>
           <span className="nav-link-title"> Event Details </span>
-        </a>
+        </Link>
       </li>
       <li className="nav-item">
-        <a className="nav-link" href="/login">
+        <Link className="nav-link" href="/login">
           <span className="nav-link-icon">
             <IconClipboardCheck></IconClipboardCheck>
           </span>
           <span className="nav-link-title"> Register </span>
-        </a>
+        </Link>
       </li>
       <li className="nav-item">
-        <a className="nav-link" href="/dashboard">
+        <Link className="nav-link" href="/dashboard">
           <span className="nav-link-icon">
             <IconLayoutDashboard></IconLayoutDashboard>
           </span>
           <span className="nav-link-title"> Contestant Dashboard </span>
-        </a>
+        </Link>
       </li>
       <li className="nav-item">
-        <a className="nav-link" href="/teamfinder">
+        <Link className="nav-link" href="/teamfinder">
           <span className="nav-link-icon">
             <IconUserSearch></IconUserSearch>
           </span>
           <span className="nav-link-title"> Team Finder </span>
-        </a>
+        </Link>
       </li>
           </ul>
       </div>
