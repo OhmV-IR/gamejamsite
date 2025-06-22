bed3e17fe25affeae203ed96d6de9ec3dba9b35d
Adrien Bourdeaux <bourdeaux.adrien@gmail.com>
2025-06-22 00:28:52 -0400

    add event details

--- app/eventdetails/page.js
+++ app/eventdetails/page.js
@@ -2,6 +2,22 @@ import styles from './page.module.css';
 
 export default function EventDetails(){
     return (
-        <h1 className={styles.centertext}>To be determined.</h1>
+        <div>
+            <h1 className={styles.centertext}>Event Details</h1>
+            <h2 className={styles.subheader}>Age Groups</h2>
+            <p className={styles.answertext}>Junior: 11-14 years old</p>
+            <p className={styles.answertext}>Senior: 14-18 years old</p>
+            <h2 className={styles.subheader}>Date</h2>
+            <p className={styles.answertext}>August 2025</p>
+            <h2 className={styles.subheader}>What to bring</h2>
+            <ul>
+                <li className={styles.answertext}>Your own device</li>
+                <li className={styles.answertext}>Charger for your device</li>
+                <li className={styles.answertext}>Your required software such as VSCode or Blender</li>
+                <li className={styles.answertext}>Food and water</li>
+            </ul>
+            <p className={styles.subheader}>Junior attendees are required to be present in-person to take part in the competition</p>
+            <p className={styles.subheader}>Senior attendees can either take part online or in-person</p>
+        </div>
     )
 }
