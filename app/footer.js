15abf3e2880c3443ad2c9838ecc2ab0624162b15
unknown <8ardrinsarelwqu3@gmail.com>
2025-06-22 19:47:58 -0400

    footer resize and slight event detail reformat

--- app/footer.js
+++ app/footer.js
@@ -1,13 +1,13 @@
 import styles from "./page.module.css";
 
-export default function Footer(){
+export default function Footer() {
     return (
         <div>
-        <div className={styles.footerpad}></div>
-        <div className="hr-text">For More Information</div>
-        <div className={styles.footerdiv}>
-            <h1>Contact us at: <a href="mailto:jambytesteam@gmail.com" className={styles.linktext}>jambytesteam@gmail.com</a></h1>
-        </div>
+            <div className={styles.footerpad}></div>
+            <div className="hr-text">Additional Information</div>
+            <div className={styles.footerdiv}>
+                <h3><a href="mailto:jambytesteam@gmail.com" className={styles.linktext}>jambytesteam@gmail.com</a></h3>
+            </div>
         </div>
     )
 }
