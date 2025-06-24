87949e3508a503b53bda9dda450f238bcdc25c6e
Adrien Bourdeaux <bourdeaux.adrien@gmail.com>
2025-06-23 22:08:43 -0400

    fixup footer

--- app/footer.js
+++ app/footer.js
@@ -4,9 +4,19 @@ export default function Footer() {
     return (
         <div>
             <div className={styles.footerpad}></div>
-            <div className="hr-text">Additional Information</div>
             <div className={styles.footerdiv}>
-                <h3><a href="mailto:jambytesteam@gmail.com" className={styles.linktext}>jambytesteam@gmail.com</a></h3>
+                <div className="hr-text">Additional Information</div>
+                <div className="row w-100">
+                    <div className="col">
+                        <h3><a href="mailto:jambytesteam@gmail.com" className={styles.linktext}>jambytesteam@gmail.com</a></h3>
+                    </div>
+                    <div className="col">
+                        <a href="/tos" className={styles.greylink}>Terms of Service</a>
+                    </div>
+                    <div className="col">
+                        <a href="/privacy" className={styles.greylink}>Privacy Policy</a>
+                    </div>
+                </div>
             </div>
         </div>
     )
