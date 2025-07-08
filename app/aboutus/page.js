fc40c78f17a1797b6e56f8bd3ef9f2ccfe546912
Ardrinsarelwqu <8ardrinsarelwqu3@gmail.com>
2025-07-08 13:39:59 -0400

    fixed aboutus mobile formatting and made card creation a function

--- app/aboutus/page.js
+++ app/aboutus/page.js
@@ -2,6 +2,24 @@
 import styles from "./page.module.css"
 import Image from "next/image";
 
+function createCard(src, style, alt, title, subtitle, dimx = 1024, dimy = 1024) {
+    return (
+        <div className="col">
+            <div className={style}>
+                <div className="card">
+                    <Image className={styles.roundImage} src={src} alt={alt} width={dimx} height={dimy}></Image>
+                    <div className="card-body-sm">
+                        <div className={styles.cardtextpadding}>
+                            <h3 className="card-title mt-2 mb-0">{title}</h3>
+                            <p className="text-secondary">{subtitle}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
 export default function AboutUs() {
     return (
         <div>
@@ -11,69 +29,19 @@ export default function AboutUs() {
             <h1 className={styles.subheader}>Mission Statement</h1>
             <h2 className={styles.text}>We strive to educate and inspire young minds to learn, create, and play by providing them with a platform to showcase their problem-solving skills through our game jam.</h2>
             <h1 className={styles.subheader}>How you can help us</h1>
-            <h2 className={styles.text}>We are always appreciative of sponsorships and volunteer offers; please send us an email at&nbsp;
-                <a className={styles.emaillink} href="mailto:jambytesteam@gmail.com">jambytesteam@gmail.com</a> to learn more about how you can get involved!</h2>
+            <h2 className={styles.text}>We are always appreciative of sponsorships and volunteer offers; please send us an email&nbsp;
+                <a className={styles.emaillink} href="mailto:jambytesteam@gmail.com">here</a> to learn more about how you can get involved!</h2>
             <h1 className={styles.subtitle}>Meet The Team</h1>
             <div className="row w-75">
-                <div className="col">
-                    <div className={styles.personcard}>
-                        <div className="card">
-                            <Image className={styles.roundImage} src="/Vishnu.jpg" alt="A portrait of Vishnu" width={1024} height={1024} ></Image>
-                            <div className="card-body">
-                                <h3 className="card-title">Vishnu</h3>
-                                <p className="text-secondary">Lead Organizer</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="col">
-                    <div className={styles.personcard}>
-                        <div className="card">
-                            <Image className={styles.roundImage} src="/Aryaman.jpeg" alt="A portrait of Aryaman" width={1024} height={1024} ></Image>
-                            <div className="card-body">
-                                <h3 className="card-title">Aryaman</h3>
-                                <p className="text-secondary">Workshop Coordinator</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                {createCard("/Vishnu.jpg", styles.personcard, "A portrait of Vishnu", "Vishnu", "Lead Organizer")}
+                {createCard("/Aryaman.jpeg", styles.personcard, "A portrait of Aryaman", "Aryaman", "Workshop Coordinator")}
             </div>
             <div className="row w-75">
-                <div className="col">
-                    <div className={styles.personcard}>
-                        <div className="card">
-                            <Image className={styles.roundImage} src="/Jared.png" alt="A portrait of Jared" width={1024} height={1024} ></Image>
-                            <div className="card-body">
-                                <h3 className="card-title">Jared</h3>
-                                <p className="text-secondary">Workshop Coordinator</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="col">
-                    <div className={styles.personcard}>
-                        <div className="card">
-                            <Image className={styles.roundImage} src="/Adrien.png" alt="A portrait of Adrien" width={1024} height={1024} ></Image>
-                            <div className="card-body">
-                                <h3 className="card-title">Adrien</h3>
-                                <p className="text-secondary">Website Manager</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                {createCard("/Jared.png", styles.personcard, "A portrait of Jared", "Jared", "Workshop Coordinator")}
+                {createCard("/Adrien.png", styles.personcard, "A portrait of Adrien", "Adrien", "Website Manager")}
             </div>
             <div className="row w-75">
-                <div className="col">
-                    <div className={styles.centrecard}>
-                        <div className="card">
-                            <Image className={styles.roundImage} src="/Maharshi.png" alt="A portrait of Maharshi" width={1024} height={1024} ></Image>
-                            <div className="card-body">
-                                <h3 className="card-title">Maharshi</h3>
-                                <p className="text-secondary">Head of Marketing</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                {createCard("/Maharshi.png", styles.centrecard, "A portrait of Maharshi", "Maharshi", "Head of Marketing")}
             </div>
         </div>
     )
