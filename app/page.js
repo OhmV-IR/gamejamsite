9e421da664465e936dce6a18fc6cb4e0d575cfaf
Adrien Bourdeaux <bourdeaux.adrien@gmail.com>
2025-07-18 22:14:44 -0600

    Fix carousel once and for all

--- app/page.js
+++ app/page.js
@@ -1,46 +1,32 @@
 "use client"
 import styles from "./page.module.css";
-import { useEffect } from "react";
-import Image from "next/image"
 
 export default function Home() {
-  useEffect(() => {
-    import('bootstrap/dist/js/bootstrap.bundle.min.js').then((bootstrap) => {
-      const carousel = document.getElementById('bgcarousel');
-      if (carousel) {
-        new bootstrap.Carousel(carousel, {
-          interval: 7500,
-          ride: 'carousel',
-          pause: false,
-        });
-      }
-    })
-  }, []);
   return (
     <div id="homepageroot">
       <div className={styles.bg}>
-        <div className={`carousel slide ${styles.bgcarousel}`} id="bgcarousel">
+        <div className={`carousel slide ${styles.bgcarousel}`} id="bgcarousel" data-bs-ride="carousel" data-bs-interval="5000" data-bs-pause="false">
           <div className="carousel-inner">
             <div className="carousel-item active">
-              <Image width={0} height={0} className={`d-block ${styles.carouselimg}`} sizes="100vw" alt="What is JamBytes?" loading="eager" src="/carouselslide1.png"></Image>
+              <img className={`d-block ${styles.carouselimg}`} alt="What is JamBytes?" src="/carouselslide1.png"></img>
             </div>
             <div className="carousel-item">
-              <Image width={0} height={0} className={`d-block ${styles.carouselimg}`} sizes="100vw" alt="Seniors (age 14-18) will have an online senior gamejam and Juniors (age 11-14) will have an in-person Junior Gamejam in Scratch" loading="eager" src="/carouselslide2.png"></Image>
+              <img className={`d-block ${styles.carouselimg}`} alt="Seniors (age 14-18) will have an online senior gamejam and Juniors (age 11-14) will have an in-person Junior Gamejam in Scratch" src="/carouselslide2.png"></img>
             </div>
             <div className="carousel-item">
-              <Image width={0} height={0} className={`d-block ${styles.carouselimg}`} sizes="100vw" alt="All participants will make games in the specified time frame which will then be judged by the JamBytes judges" loading="eager" src="/carouselslide3.png"></Image>
+              <img className={`d-block ${styles.carouselimg}`} alt="All participants will make games in the specified time frame which will then be judged by the JamBytes judges" src="/carouselslide3.png"></img>
             </div>
             <div className="carousel-item">
-              <Image width={0} height={0} className={`d-block ${styles.carouselimg}`} sizes="100vw" alt="The senior game jam will have teams chosen by the participants while the juniors will work independently" loading="eager" src="/carouselslide4.png"></Image>
+              <img className={`d-block ${styles.carouselimg}`} alt="The senior game jam will have teams chosen by the participants while the juniors will work independently" src="/carouselslide4.png"></img>
             </div>
             <div className="carousel-item">
-              <Image width={0} height={0} className={`d-block ${styles.carouselimg}`} sizes="100vw" alt="the Junior gamejam will last for a day in person while the Senior game jam will be 2 days online" loading="eager" src="/carouselslide5.png"></Image>
+              <img className={`d-block ${styles.carouselimg}`} alt="the Junior gamejam will last for a day in person while the Senior game jam will be 2 days online" src="/carouselslide5.png"></img>
             </div>
             <div className="carousel-item">
-              <Image width={0} height={0} className={`d-block ${styles.carouselimg}`} sizes="100vw" alt="There is no entrance fee for anyone to participate in either the Junior or Senior game jams" loading="eager" src="/carouselslide6.png"></Image>
+              <img className={`d-block ${styles.carouselimg}`} alt="There is no entrance fee for anyone to participate in either the Junior or Senior game jams" src="/carouselslide6.png"></img>
             </div>
             <div className="carousel-item">
-              <Image width={0} height={0} className={`d-block ${styles.carouselimg}`} sizes="100vw" alt="Sign up for JamBytes today" loading="eager" src="/carouselslide7.png"></Image>
+              <img className={`d-block ${styles.carouselimg}`} alt="Sign up for JamBytes today" src="/carouselslide7.png"></img>
             </div>
           </div>
         </div>
