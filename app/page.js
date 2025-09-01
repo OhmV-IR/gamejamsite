db6495305e5f20d5a4dcc854285d27bc59aa1329
Ardrinsarelwqu <8ardrinsarelwqu3@gmail.com>
2025-09-01 18:57:52 -0400

    fixing margins

--- app/page.js
+++ app/page.js
@@ -7,8 +7,8 @@ export default function Home() {
       <div className={styles.bg}></div>
       <div className={styles.titlebox}>
         <h1 className={styles.jamtitle}>JamBytes</h1>
-        <h2 className={styles.jamdetails}>A free-to-participate game jam for learners and experienced developers alike</h2> //
-        <h2 className={styles.jamdetails}>Top 5</h2>
+        <h2 className={styles.jamdetails}>A free-to-participate game jam for learners and experienced developers alike</h2>
+        <h2 className={styles.jamdetails2}>Top 5</h2>
         <p className={styles.jamdetails}>1. Elementrix - Hunting-Umbra (Sathvik)</p>
         <p className={styles.jamdetails}>2. Heart Defense - deepwoken (A K)</p>
         <p className={styles.jamdetails}>3. City Without Money - Bottom of the barrel (Riley)</p>
